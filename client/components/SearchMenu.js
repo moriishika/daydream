f7abf5db86e55570ad516674c407ae7f95b7ac90
Morii Shikaa <ramanavami32@gmail.com>
2021-01-05 20:14:01 +0800

    custom style select tag finished

--- client/components/SearchMenu.js
+++ client/components/SearchMenu.js
@@ -10,6 +10,7 @@ customElements.define('search-menu', class SearchMenu extends HTMLElement{
             }
             select,input{
                 height:68%;
+                font-family:'Cormorant Garamond', serif;
             }
             search-menu{
                 display:flex;
@@ -22,11 +23,11 @@ customElements.define('search-menu', class SearchMenu extends HTMLElement{
             input[type="search"] {
                 -webkit-appearance: none;
                 appearance: none;
-                border: 1px solid #575756;
+                border: 1px solid #9871c7;
                 border-radius:15px;
                 transition:all 250ms ease-in-out;
                 background-color :transparent;
-                padding: 5px 7px;
+                padding: 8px 7px;
                 backface-visibility:hidden;
                 transform-style: preserve-3d;
                 background-image: url("../../assets/svg/loupe.svg");
@@ -35,21 +36,40 @@ customElements.define('search-menu', class SearchMenu extends HTMLElement{
                 background-position: 95% center;
             }
 
-            input[type="search"]:placeholder{
-            color: #575756;
-            text-transform: uppercase;
+            input[type="search"]::placeholder{
+            color: #9871c7;
             letter-spacing: 1.5px;
+            font-size:15px;
+            font-weight: bold
             }
             
             input[type="search"]:focus,
             input[type="search"]:hover{
                 border-width:0 0 1px 0;
                 border-radius:0px;
-                padding : 5px 0px;
+                padding : 8px 0px;
                 border: 1px solid transparent;
-                border-bottom: 1px solid #575756;
+                border-bottom: 1px solid #9871c7;
                 outline:none;
                 
+            }
+
+            select{
+                border: 1px solid #9871c7;
+                border-width: 0 0 1px 0;
+                background-color:transparent;
+                color: #9871c7;
+                font-weight:bold;
+                font-size:15px;
+            }
+
+            select:focus,
+            select:hover{
+                outline:none;
+            }
+
+            option{
+                border : 0 0 1px 0;
             }
             
         </style>
@@ -61,7 +81,7 @@ customElements.define('search-menu', class SearchMenu extends HTMLElement{
                 <option value="">Low to High Price</option>
                 <option value="">High to Low Price</option>
             </select>
-            <input type="search" placeholder="search">
+            <input type="search" placeholder="Search">
         </div>
         `
     }
